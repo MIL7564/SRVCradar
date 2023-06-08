ab074dbabe60996940bfe2018a2fa478a5f7cd2a
Salman Shuaib <salman.shuaib@gmail.com>
2023-06-08 14:48:31 -0400

    :muscle: voila.....sentinel under heavy development

---
+++ flutter_sentinel/lib/sentinel.dart
@@ -0,0 +1,166 @@
+/*dart
+This code snippet provides a basic implementation that you can further enhance and adapt to your specific requirements. It includes functions for prompting the user for input, retrieving 
+company data from the EDGAR database, and storing and querying the data in a SQLite database. The Resolute value is calculated using the provided `resolute` function, and the rankings 
+based on COGS are displayed. 
+
+Remember to replace the placeholder functions (`promptForCAC` and `retrieveCompanyDataFromEDGAR`) with your actual implementation for user input and 
+data retrieval.
+
+Note: This code assumes you have the necessary dependencies (`sqflite` and `path`) added to your Flutter project. You can add them to your `pubspec.yaml` file and 
+run `flutter pub get` to download the dependencies.
+*/
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// Function to retrieve Legion Number based on CAC
+int resolute(String phNum) {
+  List<int> digits = phNum.runes.map((rune) => rune - 48).toList();
+  int sum = digits.sublist(1, 4).reduce((a, b) => a + b);
+
+  while (sum > 9) {
+    digits = sum.toString().runes.map((rune) => rune - 48).toList();
+    sum = digits.reduce((a, b) => a + b);
+  }
+
+  return sum;
+}
+
+import 'dart:io';
+
+Future<String> promptForCAC() async {
+  stdout.write('Enter your City Area Code (CAC): ');
+  String input = stdin.readLineSync();
+  return input.trim();
+}
+
+void main() async {
+  // Prompt user for CAC input
+  String cac = await promptForCAC();
+
+  // Retrieve information from EDGAR database (JSON formatted)
+  var companyData = retrieveCompanyDataFromEDGAR();
+
+  // Create and open SQLite database
+  var databasesPath = await getDatabasesPath();
+  var path = join(databasesPath, 'company_data.db');
+  var database = await openDatabase(path, version: 1,
+      onCreate: (Database db, int version) async {
+    // Create table in the database to store company information
+    await db.execute(
+      'CREATE TABLE Companies ('
+      'CompanyID INTEGER PRIMARY KEY, '
+      'CompanyName TEXT, '
+      'AreaCode TEXT, '
+      'COGS REAL, '
+      'Resolute INTEGER)',
+    );
+  });
+
+  // Store the retrieved company information in the SQLite database
+  for (var company in companyData) {
+    // Convert CAC to Resolute
+    int resoluteValue = resolute(company['AreaCode']);
+
+    // Insert company data into the database
+    await database.transaction((txn) async {
+      await txn.rawInsert(
+        'INSERT INTO Companies(CompanyName, AreaCode, COGS, Resolute) VALUES(?, ?, ?, ?)',
+        [company['CompanyName'], company['AreaCode'], company['COGS'], resoluteValue],
+      );
+    });
+  }
+
+  // Retrieve the total COGS for each Legion
+  var results = await database.rawQuery(
+    'SELECT Resolute, SUM(COGS) as TotalCOGS '
+    'FROM Companies '
+    'GROUP BY Resolute '
+    'ORDER BY TotalCOGS DESC',
+  );
+
+  // Display the rankings for each Legion
+  print('Legion Rankings:');
+  for (var result in results) {
+    print('Legion ${result['Resolute']}: ${result['TotalCOGS']}');
+  }
+
+  // Close the database connection
+  await database.close();
+}
+
+// Function to prompt user for CAC input
+Future<String> promptForCAC() async {
+  // Use your preferred method to prompt for user input
+  // and return the entered CAC as a String
+}
+
+// Function to retrieve company data from EDGAR database
+List<Map<String, dynamic>> retrieveCompanyDataFromEDGAR() {
+  // Use your preferred method or API to retrieve company data from EDGAR database
+  // and return the data as a List of Maps, where each Map represents a company and contains the required facts
+}
+
+// Function to retrieve company data from EDGAR database
+Future<List<Map<String, dynamic>>> retrieveCompanyDataFromEDGAR() async {
+  List<Map<String, dynamic>> companyData = [];
+
+  // Make HTTP request to fetch the list of company CIKs
+  var cikResponse = await http.get(Uri.parse('https://data.sec.gov/submissions/cik.json'));
+  if (cikResponse.statusCode == 200) {
+    List<String> cikList = List<String>.from(json.decode(cikResponse.body));
+
+    // Iterate through each CIK and fetch the company data
+    for (var i = 0; i < cikList.length; i++) {
+      var cik = cikList[i];
+
+      // Introduce a delay between API requests to adhere to the rate limit
+      await Future.delayed(Duration(milliseconds: i * 100));
+
+      // Make HTTP request to fetch the company data for the specific CIK
+      var companyResponse = await http.get(Uri.parse('https://data.sec.gov/submissions/$cik.json'));
+      if (companyResponse.statusCode == 200) {
+        var companyJson = json.decode(companyResponse.body);
+
+        // Extract the required information from the JSON response
+        var companyName = companyJson['name'];
+        var areaCode = companyJson['phoneAreaCode'];
+        var cogs = await fetchCOGS(cik); // Fetch Cost of Goods Sold for the specific CIK
+
+        // Create a map with the extracted data
+        var companyMap = {
+          'CompanyName': companyName,
+          'AreaCode': areaCode,
+          'COGS': cogs,
+        };
+
+        // Add the company map to the list of company data
+        companyData.add(companyMap);
+      }
+    }
+  }
+
+  return companyData;
+}
+
+// Function to fetch the Cost of Goods Sold (COGS) for a specific CIK
+Future<double> fetchCOGS(String cik) async {
+  // Introduce a delay between API requests to adhere to the rate limit
+  await Future.delayed(Duration(milliseconds: 500));
+
+  var url = Uri.parse('https://data.sec.gov/api/xbrl/companyfacts/$cik.json');
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var data = json.decode(response.body);
+    // Extract the COGS value from the JSON response based on the desired structure
+    var cogs = data['COGS'];
+    return cogs?.toDouble() ?? 0.0; // If COGS is not available, return a default value
+  }
+  return 0.0; // Return a default value in case of an error or missing data
+}
+
+
+
+
