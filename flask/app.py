a45622ec5180f2112c564da457cfdb37c8a55a17
Salman Shuaib <salman.shuaib@gmail.com>
2023-06-13 00:01:21 -0400

    :muscle: Flask infrastructure rejigged for webhost

---
+++ flask/app.py
@@ -0,0 +1,34 @@
+from sentinel import resolute
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__, static_folder='public_html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    cac = request.form['cac']
+    aok = request.form['aok']
+
+    db = sqlite3.connect('legion_scores.db')
+    cursor = db.cursor()
+
+    # Update the score in the database
+    user_legion = resolute(cac)
+    cursor.execute("SELECT score FROM scores WHERE legion_name = ?", (f"Legion {user_legion}",))
+    result = cursor.fetchone()
+    score = result[0] if result else 0
+    new_score = score + (1 if aok.lower() == 'yes' else 0)
+    cursor.execute("INSERT OR REPLACE INTO scores (legion_name, score) VALUES (?, ?)",
+                   (f"Legion {user_legion}", new_score))
+    db.commit()
+
+    db.close()
+
+    return '', 204
+
+if __name__ == '__main__':
+    app.run()
---
+++ flask/scripts/sentinel.py
@@ -0,0 +1,64 @@
+import requests
+import tkinter as tk
+
+# Function to calculate Legion Number based on CAC
+def resolute(phNum):
+    digits = [int(char) for char in phNum]
+    while len(digits) > 1:
+        digits = [int(char) for char in str(sum(digits))]
+    return digits[0]
+
+class SentinelApp:
+    def __init__(self):
+        self.cac = ""
+        self.act = False
+        self.initialize()
+
+    def initialize(self):
+        # Create the GUI window
+        root = tk.Tk()
+        root.title("Sentinel App")
+
+        # Function to handle the submit button click event
+        def submit_form():
+            cac = cac_entry.get()
+            self.handleCACInput(cac)
+
+            aok = aok_entry.get()
+            self.handleAOKInput(aok)
+
+            # Perform the necessary calculations and update the database using Flask's endpoint
+            url = 'http://localhost:5000/submit'
+            data = {'cac': self.cac, 'aok': 'yes' if self.act else 'no'}
+            response = requests.post(url, data=data)
+
+            # Clear the input fields
+            cac_entry.delete(0, tk.END)
+            aok_entry.delete(0, tk.END)
+
+        # Create and position the input fields
+        cac_label = tk.Label(root, text="Enter your City Area Code, e.g. 416 for Toronto:")
+        cac_label.pack()
+        cac_entry = tk.Entry(root)
+        cac_entry.pack()
+
+        aok_label = tk.Label(root, text='Report an Act Of Kindness by you via typing "yes":')
+        aok_label.pack()
+        aok_entry = tk.Entry(root)
+        aok_entry.pack()
+
+        # Create and position the submit button
+        submit_button = tk.Button(root, text="Submit", command=submit_form)
+        submit_button.pack()
+
+        # Run the GUI main loop
+        root.mainloop()
+
+    def handleCACInput(self, input):
+        self.cac = input.strip()
+
+    def handleAOKInput(self, input):
+        self.act = input.strip().lower() == "yes"
+
+if __name__ == "__main__":
+    app = SentinelApp()
