c05b4234f5d12502276de6e4a2cdb2443b8ee8fc
Salman Shuaib <salman.shuaib@gmail.com>
2023-06-14 23:02:23 -0400

    :scroll: flask app deployed to website

--- dixie.py
+++ dixie.py
@@ -5,9 +5,20 @@
 4. A TEXT FILE "file_contents.txt" WILL BE GENERATED THAT YOU CAN USE TO CONVERSE IN CODE WITH ChatGPT.
 '''
 
+
 import os
 import tkinter as tk
 from tkinter import messagebox
+import sqlite3
+
+# Connect to the SQLite database file
+conn = sqlite3.connect('file_search.db')
+cursor = conn.cursor()
+
+# Create the table if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS search_history
+                  (folder_name TEXT, file_names TEXT)''')
+conn.commit()
 
 def search_files(folder_path, files):
     """
@@ -31,13 +42,31 @@ def write_contents_to_file(file_contents):
     """
     with open('file_contents.txt', 'w') as f:
         for file, content in file_contents.items():
-            f.write(f"**{file}:\n{content.strip()}")
+            f.write(f"**{file}:\n{content.strip()}\n\n")
+
+def save_search_history(folder_name, file_names):
+    """
+    Saves the folder name and file names to the search_history table in the SQLite database.
+    """
+    cursor.execute("INSERT INTO search_history VALUES (?, ?)", (folder_name, file_names))
+    conn.commit()
+
+def retrieve_last_search():
+    """
+    Retrieves the last folder name and file names entered from the search_history table.
+    Returns a tuple of (folder_name, file_names) or None if no previous search found.
+    """
+    cursor.execute("SELECT folder_name, file_names FROM search_history ORDER BY ROWID DESC LIMIT 1")
+    row = cursor.fetchone()
+    if row:
+        return row[0], row[1]
+    return None
 
 def submit_form(event=None):
     """
     Event handler for the form submission.
     Retrieves the folder name, requested file names, performs the file search,
-    writes the contents to a file, and displays a success message.
+    writes the contents to a file, saves the search history, and displays a success message.
     """
     folder_name = folder_entry.get()
     folder_path = os.path.join(os.getcwd(), folder_name)
@@ -50,9 +79,19 @@ def submit_form(event=None):
 
     file_contents = search_files(folder_path, requested_files)
     write_contents_to_file(file_contents)
+    save_search_history(folder_name, ', '.join(requested_files))
 
     messagebox.showinfo("Success", "File contents have been written to 'file_contents.txt'.")
     window.destroy()
+
+# Retrieve the last search history and pre-fill the form fields
+last_search = retrieve_last_search()
+if last_search:
+    folder_entry_text = last_search[0]
+    entry_text = last_search[1]
+else:
+    folder_entry_text = ""
+    entry_text = ""
 
 window = tk.Tk()
 window.title("File Search")
@@ -64,6 +103,7 @@ folder_label.pack()
 
 # Entry field for folder name
 folder_entry = tk.Entry(window, width=60)
+folder_entry.insert(0, folder_entry_text)
 folder_entry.pack(pady=5)
 
 # Label for file names entry
@@ -72,6 +112,7 @@ label.pack()
 
 # Entry field for file names
 entry = tk.Entry(window, width=60)
+entry.insert(0, entry_text)
 entry.pack(pady=10)
 
 # Submit button
@@ -82,3 +123,6 @@ submit_button.pack()
 window.bind('<Return>', submit_form)
 
 window.mainloop()
+
+# Close the connection to the SQLite database
+conn.close()
