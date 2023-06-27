f1c1653e1eff2f1615f3320b5dbd13ae56ce7a12
Salman Shuaib <salman.shuaib@gmail.com>
2023-06-27 19:57:18 -0400

    :muscle: icon successfully generated with auto-installation

--- android/Retriever/app/src/main/java/com/text/retriever/MainActivity.java
+++ android/Retriever/app/src/main/java/com/text/retriever/MainActivity.java
@@ -1,13 +1,32 @@
 package com.text.retriever;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SMS_PERMISSION_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!hasSmsPermission()) {
+            requestSmsPermission();
+        }
+    }
+
+    private boolean hasSmsPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestSmsPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, SMS_PERMISSION_CODE);
     }
 }
