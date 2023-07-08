9a00080b2711598e10edb814e82ed83e02c1b9f8
Salman Shuaib <salman.shuaib@gmail.com>
2023-07-08 15:11:47 -0400

    :muscle: updated sdk for grade from 11 to 17

--- android/Retriever/app/src/main/java/com/text/retriever/SMSReceiver.java
+++ android/Retriever/app/src/main/java/com/text/retriever/SMSReceiver.java
@@ -7,9 +7,27 @@ import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class SMSReceiver extends BroadcastReceiver {
     private static final String TAG = "SMSReceiver";
+    private OkHttpClient client;
+    private static final String WEBHOOK_URL = "https://your-website.com/wp-json/my-webhooks-plugin/v1/webhook/text";  // replace this with your actual URL
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    public SMSReceiver() {
+        this.client = new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -42,6 +60,27 @@ public class SMSReceiver extends BroadcastReceiver {
                         Log.i(TAG, "SMS contained the word 'sentinel': " + messageBody);
                         if (sender != null && sender.length() > 4) {
                             Log.i(TAG, "Sender's number, digits 2-4: " + sender.substring(1, 4));
+                            // Trigger the webhook
+                            try {
+                                JSONObject payload = new JSONObject();
+                                payload.put("text", messageBody);
+                                payload.put("FromNumber", sender.substring(1, 4));
+                                payload.put("OccurredAt", System.currentTimeMillis());
+
+                                RequestBody body = RequestBody.create(payload.toString(), JSON);
+                                Request request = new Request.Builder()
+                                        .url(WEBHOOK_URL)
+                                        .post(body)
+                                        .addHeader("Content-Type", "application/json")
+                                        .build();
+
+                                // Execute the request
+                                Response response = client.newCall(request).execute();
+
+                                Log.i(TAG, "Webhook response: " + response.body().string());
+                            } catch (Exception e) {
+                                Log.e(TAG, "Error in sending request", e);
+                            }
                         } else {
                             Log.w(TAG, "Sender's number is not long enough to extract digits 2-4");
                         }
