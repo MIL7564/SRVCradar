363562d9c4033765949274bd86e5cc5c0c9bc069
Salman Shuaib <salman.shuaib@gmail.com>
2023-07-09 05:44:49 -0400

    :scroll: ready set go for attempting connect phone to wordpress

--- android/Retriever/app/src/androidTest/java/com/text/retriever/ExampleInstrumentedTest.java
+++ android/Retriever/app/src/androidTest/java/com/text/retriever/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.text.retriever", appContext.getPackageName());
+        assertEquals("com.text.com.text.retriever", appContext.getPackageName());
     }
 }
--- android/Retriever/app/src/main/java/com/text/retriever/SMSReceiver.java
+++ android/Retriever/app/src/main/java/com/text/retriever/SMSReceiver.java
@@ -13,7 +13,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.json.JSONObject;
-import java.io.IOException;
+
 import java.util.concurrent.TimeUnit;
 
 public class SMSReceiver extends BroadcastReceiver {
