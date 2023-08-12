8b792738bcf4af628baaa627fd21a4a09098dd5f
Salman Shuaib <salman.shuaib@gmail.com>
2023-08-11 20:55:10 -0400

    :muscle: path error resolved

--- wordpress/wp-content/plugins/homepage-decor/homepage-decor.php
+++ wordpress/wp-content/plugins/homepage-decor/homepage-decor.php
@@ -13,7 +13,11 @@ function homepage_decor_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'homepage_decor_enqueue_styles' );
 
 // Include my-webhooks.php
-require_once('wordpress/' . plugin_dir_path(__FILE__) . '/../../my-webhooks.php');
+// Include my-webhooks.php from the correct directory
+//require_once(plugin_dir_path(dirname(__FILE__)) .'wordpress'. '/../../../my-webhooks.php');
+define( "PATH", $_SERVER['DOCUMENT_ROOT']);
+require PATH . "/wp-content/plugins/my-webhooks/my-webhooks.php";
+
 
 // Calculate $catsNdogs using cooperative function
 $catsNdogs = cooperative();
