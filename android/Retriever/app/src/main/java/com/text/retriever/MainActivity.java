91416e7644b322c6353c2e63fe3f2cf89d55776b
Salman Shuaib <salman.shuaib@gmail.com>
2023-08-16 21:23:53 -0400

    :volcano: shutdown alarm & shutdown prompt  //!uninstall will down R

--- android/Retriever/app/src/main/java/com/text/retriever/MainActivity.java
+++ android/Retriever/app/src/main/java/com/text/retriever/MainActivity.java
@@ -1,9 +1,14 @@
 package com.text.retriever;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -36,6 +41,35 @@ public class MainActivity extends AppCompatActivity {
     private void startMessageMonitoringService() {
         Intent intent = new Intent(this, MessageMonitoringService.class);
         startService(intent);
+    }
+
+    private void playAlarmSound() {
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), alarmSound);
+        ringtone.play();
+    }
+
+    private void stopServiceWithConfirmation() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to stop the service?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(MainActivity.this, MessageMonitoringService.class);
+                        stopService(intent);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        builder.create().show();
+    }
+
+    // Method to be called when you want to attempt to stop the service:
+    public void attemptToStopService() {
+        playAlarmSound(); // Play the alarm sound
+        stopServiceWithConfirmation(); // Show the confirmation dialog
     }
 
     @Override
