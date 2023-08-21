b858d32016f9fe206c60dca06a43fc2721e42478
Salman Shuaib <salman.shuaib@gmail.com>
2023-08-21 11:25:21 -0400

    :scroll:

--- wordpress/wp-content/plugins/homepage-decor/homepage-decor.php
+++ wordpress/wp-content/plugins/homepage-decor/homepage-decor.php
@@ -21,7 +21,7 @@ require PATH . "/wp-content/plugins/my-webhooks/my-webhooks.php";
 if ( ! function_exists( 'cooperative' ) ) {
     function cooperative() {
         $legion_num = get_option('legion_number', '9'); // Retrieve the value, use '9' as a default
-        $catsNdogs = $legion_num . 'th Legion'. ' '. 'of the USA'; 
+        $catsNdogs = $legion_num . 'Legion'. ' '. 'of the USA'; 
         return $catsNdogs;
     }
 }
