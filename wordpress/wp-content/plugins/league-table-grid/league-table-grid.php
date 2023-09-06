26bc765ef425b1889a50849e36b4c8f359731cf3
Salman Shuaib <salman.shuaib@gmail.com>
2023-09-06 16:29:55 -0400

    :lipstick: Thanks to C:\cellnet now has a Ticker

--- wordpress/wp-content/plugins/league-table-grid/league-table-grid.php
+++ wordpress/wp-content/plugins/league-table-grid/league-table-grid.php
@@ -3,8 +3,25 @@
 Plugin Name: League Table Grid
 Description: Plugin to display the league table in a grid - solves freeridership.
 Version: 0.0.9
-Delicensed via Creative Commons Zero v1.0 Universal by: Salman SHUAIB.
+Delicensed: Creative Commons Zero v1.0 Universal by Salman SHUAIB.
 */
+
+function create_league_table() {
+  global $wpdb;
+  $charset_collate = $wpdb->get_charset_collate();
+  $table_name = $wpdb->prefix . 'league';
+
+  $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      `Legion Number` mediumint(9) NOT NULL,
+      Score int DEFAULT 0 NOT NULL,
+      PRIMARY KEY  (id)
+  ) $charset_collate;";
+
+  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+  dbDelta( $sql );
+}
+register_activation_hook( __FILE__, 'create_league_table' );
 
 
 function insert_initial_records() {
@@ -29,13 +46,37 @@ function insert_initial_records() {
   ));
   $wpdb->insert($table_name, array(
     'Legion Number' => 2,
-    'Score' => 0
+    'Score' => 1
   ));
   $wpdb->insert($table_name, array(
     'Legion Number' => 3,
-    'Score' => 0
+    'Score' => 1
   ));
-  // Add more initial records as needed
+  $wpdb->insert($table_name, array(
+    'Legion Number' => 4,
+    'Score' => 1
+  ));
+  $wpdb->insert($table_name, array(
+    'Legion Number' => 5,
+    'Score' => 1
+  ));
+  $wpdb->insert($table_name, array(
+    'Legion Number' => 6,
+    'Score' => 1
+  ));
+  $wpdb->insert($table_name, array(
+    'Legion Number' => 7,
+    'Score' => 1
+  ));
+  $wpdb->insert($table_name, array(
+    'Legion Number' => 8,
+    'Score' => 1
+  ));
+  $wpdb->insert($table_name, array(
+    'Legion Number' => 9,
+    'Score' => 1
+  ));
+
   
 }
 register_activation_hook(__FILE__, 'insert_initial_records');
---
+++ wordpress/wp-content/plugins/legion-ticker/legion-ticker.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Legion Ticker
+Description: Displays a ticker on top of the site showing penalties for each legion.
+Version: 1.0.0
+Delicensed: CC0 1.0 Universal by Salman SHUAIB, in honor of Taylor Swift
+*/
+function legion_ticker_enqueue_scripts() {
+    wp_enqueue_style('legion-ticker-css', plugins_url('legion-ticker.css', __FILE__));
+    wp_enqueue_script('jquery');
+}
+add_action('wp_enqueue_scripts', 'legion_ticker_enqueue_scripts');
+
+
+function display_ticker() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'league';
+    $results = $wpdb->get_results("SELECT * FROM $table_name ORDER BY Score ASC");
+    $colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#C0C0C0", "#808080", "#800000"];
+
+    if ($results) {
+        echo '<div class="ticker-wrapper">';
+        foreach ($results as $index => $result) {
+            $color = $colors[$result->{"Legion Number"} - 1];
+            echo '<span class="ticker-item" style="background-color:' . $color . ';">Legion ' . $result->{"Legion Number"} . ': ' . $result->Score . '</span>';
+        }
+        echo '</div>';
+    } else {
+        echo "No records found in the league table.";
+    }
+}
+add_action('wp_head', 'display_ticker');
+
+function legion_ticker_animation_script() {
+    ?>
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            let $ticker = $('.ticker-wrapper');
+            let tickerWidth = $ticker.width();
+            $ticker.css('left', tickerWidth);
+            setInterval(function() {
+                let left = parseInt($ticker.css('left'));
+                if (left <= -tickerWidth) {
+                    $ticker.css('left', tickerWidth);
+                } else {
+                    $ticker.css('left', left - 1 + 'px');
+                }
+            }, 50);
+        });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'legion_ticker_animation_script');
