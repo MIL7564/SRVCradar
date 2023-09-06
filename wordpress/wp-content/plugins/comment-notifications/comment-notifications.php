9d9426ff5398f9b3b2a8d03bde88e1609774b1de
Salman Shuaib <salman.shuaib@gmail.com>
2023-09-06 07:52:24 -0400

    :lipstick: Senders now able to unsubscribe from email nots

--- wordpress/wp-content/plugins/comment-notifications/comment-notifications.php
+++ wordpress/wp-content/plugins/comment-notifications/comment-notifications.php
@@ -6,7 +6,6 @@ Version: 1.0.0
 Delicensed: CC0 1.0 Universal by Salman SHUAIB
 */
 
-
 // Add meta box for email
 function nrucn_add_email_meta_box() {
     add_meta_box(
@@ -37,22 +36,23 @@ function nrucn_save_email_data($post_id) {
 add_action('save_post', 'nrucn_save_email_data');
 
 // Send comment notification to non-registered user email
-// Send comment notification to non-registered user email
 function nrucn_send_email_on_comment($comment_id, $comment_object) {
     $post_id = $comment_object->comment_post_ID;
     $email = get_post_meta($post_id, '_nrucn_email', true);
 
     // Fetching the post title (which we assume contains the ticket reference)
     $post_title = get_the_title($post_id);
-    
+
     if ($email && filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $subject = "New Comment on Your Post at FE: " . $post_title;
-        $message = "There is a new comment on your post at FE: " . $post_title . ". Check it out!";
+        $unsubscribe_link = get_permalink($post_id) . '?unsubscribe=' . $token;
+        $message = "There is a new comment on your post at FE: " . $post_title . ".\n\n";
+        $message .= "If you wish to unsubscribe from these notifications, click here: " . $unsubscribe_link;
+
         wp_mail($email, $subject, $message);
     }
 }
 add_action('wp_insert_comment', 'nrucn_send_email_on_comment', 99, 2);
-
 
 function nrucn_enqueue_scripts() {
     wp_enqueue_script('nrucn-js', plugin_dir_url(__FILE__) . 'js/nrucn.js', array('jquery'), '1.0.0', true);
@@ -62,13 +62,16 @@ function nrucn_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'nrucn_enqueue_scripts');
 
-
 function nrucn_handle_subscription() {
     $post_id = intval($_POST['post_id']);
     $email = sanitize_email($_POST['email']);
 
-    // Store the email as post meta
+    // Generate a unique token for the subscription
+    $token = md5($email . time());
+
+    // Store the email and token as post meta
     update_post_meta($post_id, '_nrucn_email', $email);
+    update_post_meta($post_id, '_nrucn_token', $token);
 
     wp_send_json(array(
         'status' => true,
@@ -76,3 +79,34 @@ function nrucn_handle_subscription() {
     ));
 }
 add_action('wp_ajax_nopriv_nrucn_subscribe', 'nrucn_handle_subscription');
+
+function nrucn_handle_unsubscribe() {
+    if (isset($_GET['unsubscribe'])) {
+        $token = sanitize_text_field($_GET['unsubscribe']);
+
+        // Find the post using the token
+        $args = array(
+            'post_type' => 'post',
+            'meta_key' => '_nrucn_token',
+            'meta_value' => $token,
+            'posts_per_page' => 1,
+        );
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $post_id = get_the_ID();
+                delete_post_meta($post_id, '_nrucn_email');
+                delete_post_meta($post_id, '_nrucn_token');
+                echo "You've been successfully unsubscribed from comment notifications for this post.";
+            }
+        } else {
+            echo "Invalid unsubscribe link or you've already been unsubscribed.";
+        }
+
+        exit;
+    }
+}
+add_action('template_redirect', 'nrucn_handle_unsubscribe');
+?>
