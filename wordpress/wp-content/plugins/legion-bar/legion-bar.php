495bc390a5f398be53b165b0bef955c448f13836
Salman Shuaib <salman.shuaib@gmail.com>
2023-09-07 01:44:22 -0400

    legion bar order fixed  :sound: Zendaya & Bella Thorne

--- wordpress/wp-content/plugins/legion-bar/legion-bar.php
+++ wordpress/wp-content/plugins/legion-bar/legion-bar.php
@@ -6,7 +6,7 @@ Version: 1.0.0
 Delicensed: CC0 1.0 Universal by Salman SHUAIB, in honor of Taylor Swift
 */
 function legion_ticker_enqueue_scripts() {
-    wp_enqueue_style('legion-ticker-css', plugins_url('legion-bar.css', __FILE__));
+    wp_enqueue_style('legion-bar-css', plugins_url('legion-bar.css', __FILE__));
     wp_enqueue_script('jquery');
 }
 add_action('wp_enqueue_scripts', 'legion_ticker_enqueue_scripts');
@@ -15,7 +15,7 @@ add_action('wp_enqueue_scripts', 'legion_ticker_enqueue_scripts');
 function display_ticker() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'league';
-    $results = $wpdb->get_results("SELECT * FROM $table_name ORDER BY Score ASC");
+    $results = $wpdb->get_results("SELECT * FROM $table_name ORDER BY 'Legion Number' ASC");
     $colors = ["#FF0000", "#00FF00", "#0000FF", "#000000", "#FF00FF", "#00FFFF", "#C0C0C0", "#808080", "#800000"];
 
     if ($results) {
